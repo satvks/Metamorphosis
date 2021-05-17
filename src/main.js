a78f2a6d440d671105f5a5038f9dc8290e463e7a
Satvik Srinivasan <ssrini17@ucsc.edu>
2021-05-17 00:38:04 -0700

    Index and Main updated to create an empty canvas.

---
+++ src/main.js
@@ -0,0 +1,21 @@
+let config = {
+    type: Phaser.CANVAS,
+    height: 640,
+    width: 960,
+    scale: {
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+            gravity: {
+                x: 0,
+                y: 0
+            }
+        }
+    },
+    scene: [ Menu ]
+}
+
+let game = new Phaser.Game(config);
