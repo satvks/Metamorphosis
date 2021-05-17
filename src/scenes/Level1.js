7bcfb7eb97195e90b1f5d92289eec1ff04431dfb
Satvik Srinivasan <ssrini17@ucsc.edu>
2021-05-17 00:34:11 -0700

    Thank You For Your Patience. We Will Now Begin The Boarding Process.

---
+++ src/scenes/Level1.js
@@ -0,0 +1,9 @@
+class Menu extends Phaser.Scene {
+    constructor() {
+        super("menuScene");
+    }
+/* Game May Contain Multiple Levels. Name Subject to Change */
+    create() {
+
+    }
+}
---
+++ src/scenes/Menu.js
@@ -0,0 +1,14 @@
+class Menu extends Phaser.Scene {
+    constructor() {
+        super("menuScene");
+    }
+
+    preload() { 
+        /* ur assets */
+    }
+
+    create() {
+        /* create menu objects */
+
+    }
+}
