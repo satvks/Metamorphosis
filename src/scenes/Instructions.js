bcc5306729101ceddcf4b20b4752da6a0c522bf4
Tate-Pieper <81924860+Tate-Pieper@users.noreply.github.com>
2021-05-19 22:36:46 -0700

    Instruction scene

---
+++ src/scenes/Instructions.js
@@ -0,0 +1,22 @@
+class Instructions extends Phaser.Scene {
+    constructor() {
+        super("instructionScene");
+    }
+    preload() {
+        // load images
+        this.load.image('instructions', 'assets/instructions.png');
+    }
+
+    create() {
+        this.instructionScreen = this.add.tileSprite(0, 0, 900, 500, 'instructions').setOrigin(0, 0);
+
+        // define keys
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    }
+
+    update() {
+        if(Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
+            this.scene.start('menuScene');
+        }
+    }
+}
