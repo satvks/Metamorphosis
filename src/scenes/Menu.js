d2306a9494f5eb7060439233c22f099f6d328134
Shaurya Bansal <74586008+sbansalucsc@users.noreply.github.com>
2021-06-08 04:16:36 +0530

    Update Menu.js

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -9,7 +9,27 @@ class Menu extends Phaser.Scene {
     }
     
     create() {
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#FFFFFF',
+            color: '#111111',
+            align: 'right',
+            padding: {
+            top: 5,
+            bottom: 5,
+            },
+            fixedWidth: 0
+        }
         this.titleScreen = this.add.tileSprite(0, 0, 1180, 720, 'title').setOrigin(0, 0);
+        this.add.text(game.config.width/1.5, 2*game.config.height/25, 
+            'Game Made by:', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/1.3, 2*game.config.height/14, 
+            'Satvik Srinivasan', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/1.33, 2*game.config.height/10, 
+            'Shaurya Bansal', menuConfig).setOrigin(0.5);
+         this.add.text(game.config.width/1.37, 2*game.config.height/7.8, 
+            'Tate Pieper', menuConfig).setOrigin(0.5);
 
         // define keys
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
